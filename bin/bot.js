ce51ee125c9d1ed78d4941404f313181c6788b1e
Ben Halverson <benhalverson33@gmail.com>
2015-09-17 02:50:34 -0700

    setup config.js

--- bin/bot.js
+++ bin/bot.js
@@ -1,14 +1,14 @@
 'use strict';
 var NorrisBot = require('../lib/norrisbot');
 var config = require('config3');
-var token = process.env.BOT_API_KEY;
-var dbPath = process.env.BOT_DB_PATH;
-var name = process.env.BOT_NAME;
+var token = config.token || process.env.BOT_API_KEY;
+var dbPath = config.dbPath || process.env.BOT_DB_PATH;
+var name = config.name || process.env.BOT_NAME;
 
 var norrisbot = new NorrisBot({
-  token: config.token,
-  dbPath: config.dbPath,
-  name: config.name
+  token: token,
+  dbPath: dbPath,
+  name: name
 });
 
 norrisbot.run();
